fc4a506f522b3819aeca2f6f45754b609393fe98
ASHAIN <ashain760@gmail.com>
2026-03-03 21:55:35 +0530

    Changes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,10 +5,11 @@ const config = {
     user: process.env.DB_USER || "",
     password: process.env.DB_PASSWORD || "",
     server: process.env.DB_SERVER || "",
+    port: Number(process.env.DB_PORT) || 1433,
     database: process.env.DB_DATABASE || "",
     options: {
-        encrypt: process.env.DB_ENCRYPT === "true", // for production
-        trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE === "true", // local dev
+        encrypt: process.env.DB_ENCRYPT === "true",
+        trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE === "true",
     },
     pool: {
         max: Number(process.env.DB_POOL_MAX) || 10,
