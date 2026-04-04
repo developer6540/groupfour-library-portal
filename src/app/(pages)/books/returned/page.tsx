41b2816a9fd4515a42525d6c6fdec6b34104bcbc
ASHAIN <ashain760@gmail.com>
2026-04-04 12:53:35 +0530

    changes

--- src/app/(pages)/books/returned/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ReturnedBooks from "@/components/pages/Books/ReturnedBooks";
-
-export default function ReturnedBooksPage() {
-    return <ReturnedBooks />;
-}
--- src/lib/exceptions.ts
+++
@@ -1,5 +0,0 @@
-export function throwException(message: string, status = 500) {
-    const error = new Error(message);
-    (error as any).status = status;
-    throw error;
-}
